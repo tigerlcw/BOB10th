9226e0e73d1329839e529f1220f4aae450ef8e8a
LINKER <temp2temp@daum.net>
2021-08-03 23:47:22 +0900

    0803

--- edit.py
+++ edit.py
@@ -1,5 +1,8 @@
-import os 
+import os
+from time import sleep
 
-os.system('VBoxManage startvm boan_sol --type gui')
-
+os.system('VBoxManage startvm boan_sol_win --type gui')
 #VBoxManage startvm boan_sol --type gui   #(gui | headless | sdi | separate)
+
+sleep(90) # vbox on time...wait
+os.system('VBoxManage debugvm "boan_sol_win" dumpvmcore --filename ./bosol/test_sleep.vmem')
