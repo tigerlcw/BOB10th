b23aa0d319933e39f1f3a73d8064c17e4e97b859
LINKER <temp2temp@daum.net>
2021-08-07 00:16:38 +0900

    0807

--- edit.py
+++ edit.py
@@ -1,8 +1,13 @@
 import os
 from time import sleep
 
-os.system('VBoxManage startvm boan_sol_win --type gui')
+'''해당 부분은 VBOX 조정'''
+
+#os.system('VBoxManage startvm boan_sol_win --type gui')
 #VBoxManage startvm boan_sol --type gui   #(gui | headless | sdi | separate)
 
-sleep(90) # vbox on time...wait
-os.system('VBoxManage debugvm "boan_sol_win" dumpvmcore --filename ./bosol/test_sleep.vmem')
+os.system('VBoxManage guestcontrol boan_sol_win copyto --target-directory C:\\test\ C:\\Users\\LINKER\\Desktop\\PythonWorkspace\\test_sleep.vmem --username test --password 1234 --verbose')
+
+#os.system('VBoxManage debugvm "boan_sol_win" dumpvmcore --filename ./bosol/test_sleep.vmem') 
+
+'''이 부분부터는 와이어샤크 조종'''
