7549f856172a2b844e34ee6aff5647b20ff020bf
LINKER <temp2temp@daum.net>
2021-08-12 05:49:02 +0900

    0811 code fix and snapshot func add

--- procmon_csv.py
+++ procmon_csv.py
@@ -1,4 +1,6 @@
 # pip3 install -r requirements.txt 설정 파일 인스톨하기
+# exe 및 행위가 추출 가능한 형식만 지금은 작동
+# 무조건 슬립을 줘야 안 꼬임 나중에 시스템 쪽 코드를 작성 하는 것이 좋아 보임.
 import os
 from time import sleep
 import pandas as pd
@@ -9,42 +11,49 @@ import numpy as np
 import re
 
 
-print("분석할 파일이름을 입력하세요. ")
-file_name = input()
-
 '''해당 부분은 VBOX 조정'''
 # gui / headless - 백그라운드 실행
-#os.system('VBoxManage startvm boan_sol_win --type headless --username test --password 1234 --verbose')
-#os.system('VBoxManage controlvm boan_sol_win --type headless addencpassword --username test --password 1234 --verbose')
-#sleep(55)
+#os.system('VBoxManage startvm boan_sol_win --type headless')
+#sleep(60)
+file_name = input("분석할 파일이름을 입력하세요. -> ")
+add_func = input("테스트용 명령어 인자를 추가해주세요. 없으면 Enter Ex) /all -> ")
+
 
 # 호스트에서 게스트로 파일 전송
 
 os.system('VBoxManage guestcontrol boan_sol_win copyto --target-directory C:\\test C:\\Users\\LINKER\\Desktop\\PythonWorkspace\\bosol\\%s --username test --password 1234 --verbose' % file_name)
+sleep(2)
+os.system('VBoxManage snapshot boan_sol_win take win-test') # 스냅샷 찍기
+sleep(10)
 
 '''이 부분부터는 프로세스모니터 조종'''
-# 무조건 슬립을 줘야 안 꼬임 나중에 시스템 쪽 코드를 작성 하는 것이 좋아 보임.
 
-os.system('start /b VBoxManage guestcontrol boan_sol_win run --exe "C:\windows\system32\cmd.exe" --username test --password 1234 --wait-stdout -- emd.exe /c Procmon64 /BackingFile C:\\test\\kkabi.pml')
-sleep(2)
-os.system('start /b VBoxManage guestcontrol boan_sol_win run --exe "C:\windows\system32\cmd.exe" --username test --password 1234 --wait-stdout -- emd.exe /c C:\\test\\cmd.exe')
+os.system('start /b VBoxManage guestcontrol boan_sol_win run --exe "C:\Windows\SysWOW64\cmd.exe" --username test --password 1234 /c Procmon64 /BackingFile C:\\test\\monitor.pml')
+sleep(5)
+os.system('start /b VBoxManage guestcontrol boan_sol_win run --exe "C:\Windows\SysWOW64\cmd.exe" --username test --password 1234 /c C:\\test\\%s /c %s' % (file_name, add_func))
 sleep(4)
-os.system('start /b VBoxManage guestcontrol boan_sol_win run --exe "C:\windows\system32\cmd.exe" --username test --password 1234 --wait-stdout -- emd.exe /c Procmon64 /Terminate')
-sleep(8)
-os.system('start /b VBoxManage guestcontrol boan_sol_win run --exe "C:\windows\system32\cmd.exe" --username test --password 1234 --wait-stdout -- emd.exe /c Procmon64 /OpenLog C:\\test\\kkabi.pml /SaveAs C:\\test\\kkabi.csv')
+os.system('start /b VBoxManage guestcontrol boan_sol_win run --exe "C:\Windows\SysWOW64\cmd.exe" --username test --password 1234 /c Procmon64 /Terminate')
+sleep(8) #ipconfig 8sec
+os.system('start /b VBoxManage guestcontrol boan_sol_win run --exe "C:\Windows\SysWOW64\cmd.exe" --username test --password 1234 /c Procmon64 /OpenLog C:\\test\\monitor.pml /SaveAs C:\\test\\monitor.csv')
 sleep(2)
 print("no error step 1")
-os.system('VBoxManage guestcontrol boan_sol_win copyfrom --target-directory C:\\Users\\LINKER\\Desktop\\PythonWorkspace\\bosol\\monitor.csv C:\\test\\kkabi.csv --username test --password 1234 --verbose')
-
+os.system('VBoxManage guestcontrol boan_sol_win copyfrom --target-directory C:\\Users\\LINKER\\Desktop\\PythonWorkspace\\bosol\\ C:\\test\\monitor.csv --username test --password 1234 --verbose')
 '''이 부분부터는 와이어샤크 조종'''
-
+sleep(2)
 # 1. 와샥키고 -> 2. 이더넷 고르고 3. 패킷 다른이름 저장누르고 4. 경로 설정 및 이름 설정
 # -c 20 -> 20개 패킷 
 # 게스트에서 tshark 명령 실행 후 바로 호스트 피시로 결과.csv 파일이 전달 옴 ㄱㅇㄷ
-os.system('VBoxManage guestcontrol boan_sol_win run --exe "C:\windows\system32\cmd.exe" --username test --password 1234 --wait-stdout -- cmd.exe /c tshark -i eth0 -T fields -E separator=, -E quote=d -e _ws.col.No. -e _ws.col.Time -e _ws.col.Source -e _ws.col.Destination -e _ws.col.Protocol -e _ws.col.Length -e _ws.col.Info -c 20' + '> C:\\Users\\LINKER\\Desktop\\PythonWorkspace\\bosol\\kkabi.csv')
+os.system('VBoxManage guestcontrol boan_sol_win run --exe "C:\Windows\SysWOW64\cmd.exe" --username test --password 1234 --wait-stdout -- cmd.exe /c tshark -i eth0 -T fields -E separator=, -E quote=d -e _ws.col.No. -e _ws.col.Time -e _ws.col.Source -e _ws.col.Destination -e _ws.col.Protocol -e _ws.col.Length -e _ws.col.Info -c 10' + '> C:\\Users\\LINKER\\Desktop\\PythonWorkspace\\bosol\\shark.csv')
 print("성공")
-#tshark -i eth0 -T fields -E separator=, -E quote=d -e _ws.col.No. -e _ws.col.Time -e _ws.col.Source -e _ws.col.Destination -e _ws.col.Protocol -e _ws.col.Length -e _ws.col.Info > C:\test\ls.csv
+sleep(2)
 
+os.system('VBoxManage controlvm boan_sol_win poweroff') #power off vm
+sleep(15)
+print("꺼짐완료")
+os.system('VBoxManage snapshot boan_sol_win restore win-test') #스냅샷 시점으로 복원
+sleep(10)
+print("스냅샷복원")
+os.system('VBoxManage startvm boan_sol_win --type gui')
 
 
 # 특정 프로그램 실행 후 30초동안의 log 수집한 csv?
